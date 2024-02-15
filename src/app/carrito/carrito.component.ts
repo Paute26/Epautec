38707bc43f21bf89a51633589206648d7beb93a0
Paute26 <paul.paute26@outlook.com>
2024-02-14 19:14:45 -0500

    comprobacion de usuario, add and delete productos

--- src/app/carrito/carrito.component.ts
+++ src/app/carrito/carrito.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; 
 import { CarritoService } from '../Servicios/carrito.service';
+import { UsuariosService } from '../Servicios/usuarios.service';
+import { Usuario } from '../domain/usuario';
 import { Producto } from '../domain/producto';
 
 @Component({
@@ -9,10 +12,16 @@ import { Producto } from '../domain/producto';
 })
 export class CarritoComponent implements OnInit {
   productosEnCarrito: Producto[] = [];
+  usuario: Usuario | null = null; // Agrega la propiedad usuario
 
-  constructor(private carritoService: CarritoService) { }
+  constructor(
+    private carritoService: CarritoService,
+    private usuariosService: UsuariosService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.verificarAutenticacion(); // Verifica la autenticación al iniciar el componente
     this.obtenerProductosEnCarrito();
   }
 
@@ -25,5 +34,30 @@ export class CarritoComponent implements OnInit {
   eliminarProducto(producto: Producto) {
     this.carritoService.eliminarProductoDelCarrito(producto);
     console.log('Eliminacion exitosa');
+  }
+
+  verificarAutenticacion() {
+    this.usuariosService.getUsuario().subscribe(
+      (usuario: Usuario | null) => {
+        if (usuario && Object.keys(usuario).length !== 0) {
+          this.usuario = usuario; // Asigna el usuario obtenido del servicio si existe información dentro del objeto
+        } else {
+          this.usuario = null; // Si el usuario está vacío, establece usuario como null
+        }
+      },
+      error => {
+        console.error('Error al obtener la información del usuario: ', error);
+      }
+    );
+  }
+  
+  
+
+  iniciarSesion() {
+    this.router.navigate(['Acceso']); 
+  }
+
+  realizarCompra() {
+    // Aquí puedes agregar la lógica para realizar la compra
   }
 }
